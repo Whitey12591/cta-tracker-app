f14347006dc75fb0eb151d5e621d7e848dec9882
Daniel White <danielwhite@Daniels-MacBook-Pro.local>
2019-06-02 15:51:19 -0500

    Updates

---
+++ precache-manifest.bc74627bd323945409184a27eba4095f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e25478bece1be11fc61870838eb1159",
+    "url": "/cta-tracker-app/index.html"
+  },
+  {
+    "revision": "b5c122fe79a2b08ad909",
+    "url": "/cta-tracker-app/static/css/main.45ca7a1f.chunk.css"
+  },
+  {
+    "revision": "295e66b32a25fdafc38b",
+    "url": "/cta-tracker-app/static/js/2.a487d4a1.chunk.js"
+  },
+  {
+    "revision": "b5c122fe79a2b08ad909",
+    "url": "/cta-tracker-app/static/js/main.9efd4337.chunk.js"
+  },
+  {
+    "revision": "94f59002f58fa26318be",
+    "url": "/cta-tracker-app/static/js/runtime~main.d9195130.js"
+  }
+]);
